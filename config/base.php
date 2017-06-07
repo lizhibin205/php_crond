215d7dca2afac457e6005cf5eb469d38f32e46b9
lizhibin <lizhibin@localhost>
2017-06-07 17:23:23 +0800

    添加http_server配置

--- config/base.php
+++ config/base.php
@@ -6,4 +6,10 @@ return [
     'log_file' => PROJECT_ROOT . "/logs/crond.log",
     //主进程pid文件
     'pid_file' => PROJECT_ROOT . "/logs/crond.pid",
+    //http server
+    'http_server' => [
+        'switch' => true,
+        'listen' => '127.0.0.1',
+        'port' => 8080,
+    ],
 ];
