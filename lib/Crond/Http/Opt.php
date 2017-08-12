316cbd2e43213e5394d0993928e0b72d272c8760
lizhibin <331313869@qq.com>
2017-08-12 18:58:48 +0800

    添加Http：重新加载任务列表文件

--- lib/Crond/Http/Opt.php
+++ lib/Crond/Http/Opt.php
@@ -11,6 +11,25 @@ class Opt
     {
         $pid = \Crond\Task\Main::getPid();
         if (is_numeric($pid)) {
+            posix_kill($pid, SIGUSR1);
+            return [
+                'msg' => 'signal USR1 sent!'
+            ];
+        } else {
+            return [
+                'msg' => 'error: pid file lost!'
+            ];
+        }
+    }
+
+    /**
+     * 重新加载任务列表
+     * @return array
+     */
+    public function reload()
+    {
+        $pid = \Crond\Task\Main::getPid();
+        if (is_numeric($pid)) {
             posix_kill($pid, SIGUSR2);
             return [
                 'msg' => 'signal USR2 sent!'
