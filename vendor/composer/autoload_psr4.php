7f95c37c8d00ecff505cc818b82fe883ca129508
zhibin02.li <zhibin02.li@vipshop.com>
2018-10-17 15:23:14 +0800

    addd guzzlehttp

--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -19,4 +19,7 @@ return array(
     'Psr\\Log\\' => array($vendorDir . '/psr/log/Psr/Log'),
     'Psr\\Http\\Message\\' => array($vendorDir . '/psr/http-message/src'),
     'Monolog\\' => array($vendorDir . '/monolog/monolog/src/Monolog'),
+    'GuzzleHttp\\Psr7\\' => array($vendorDir . '/guzzlehttp/psr7/src'),
+    'GuzzleHttp\\Promise\\' => array($vendorDir . '/guzzlehttp/promises/src'),
+    'GuzzleHttp\\' => array($vendorDir . '/guzzlehttp/guzzle/src'),
 );
